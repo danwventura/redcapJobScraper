a264185d5c84564f8c3ad5225cc9ca9ded9c6ab0
danwventura <danwventura@gmail.com>
2024-09-19 13:09:47 -0400

    Setting up test object

---
+++ hospitals.py
@@ -0,0 +1,15 @@
+class Hospital:
+    def __init__(self, name, city, url, jobs):
+        self.name = name
+        self.city = city
+        self.url = url
+        # self.elementToScrape = elementToScrape
+        # FULL LIST VVVV
+        # self.keywords = ["data", "redcap", "tableau", "it", "developer", "programmer", "redcap administrator", "data manager", "database analyst", "data integration", "data integration specialist", "research database specialist", "data quality", "programmer analyst", "research it support"]
+         # TEST LIST VVVV
+        self.keywords = ["nurse", "manager"]
+        self.jobs = jobs
+
+UHN = Hospital("University Health Network", "Toronto","https://forms.uhn.ca/UHNCareers", ["test"])
+
+print(UHN.__dict__)
---
+++ scraper.py
@@ -0,0 +1,2 @@
+# MVP: A web scraper that will tell me the name of positions at all hospitals in my list that list 80% of REDCap jobs in Canada
+
